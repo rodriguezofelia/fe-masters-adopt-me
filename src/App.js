61e6099394b3ba38b2ce661d52ae09163d664e20
Ofelia <oer.rodriguez@gmail.com>
2021-11-09 16:22:47 -0600

    using useState to update location of search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Fuzz" animal="Dog" breed="Lab" />
-      <Pet name="Peper" animal="Bird" breed="Cockatiel" />
+      <SearchParams />
     </div>
   );
 };
