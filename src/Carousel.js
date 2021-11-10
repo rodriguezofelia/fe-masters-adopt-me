b0c5254623dbf91d72e9d52697fad5b57e62d77c
Ofelia <oer.rodriguez@gmail.com>
2021-11-10 17:36:04 -0600

    making carousel imgs clickable

--- src/Carousel.js
+++ src/Carousel.js
@@ -10,6 +10,12 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     // getting images from parent Pet component
@@ -20,9 +26,12 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // this should be a button since due to onclick but just go with it for now
             <img
               key={photo}
               src={photo}
+              data-index={index}
+              onClick={this.handleIndexClick}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
