fd82d594f5f1b1f9bfbf0ddf98282d79193fbab9
Ofelia <oer.rodriguez@gmail.com>
2021-11-11 10:25:05 -0600

    adding a redirect for error

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,24 +1,28 @@
 // taken from React docs
 
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-  state = { hasError: false };
+  state = { hasError: false, redirect: false };
   static getDerivedStateFromError(e) {
     return { hasError: true };
   }
   componentDidCatch(error, info) {
     // log to Sentry, etc
     console.error("ErrorBoundary caught an error", error, info);
+    setTimeout(() => this.setState({ redirect: true }), 5000);
   }
+
   render() {
     // if there is an error click here to go to the home page
     if (this.state.hasError) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
           This listing has an erorr. <Link to="/">Click here</Link> to go back
-          to the home Page.
+          to the home Page or wait five seconds.
         </h2>
       );
     }
